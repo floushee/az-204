238b0025743edfab3986986b16d0c0ab4f25cf55
floushee <dominik.deutsch@gmail.com>
2020-08-18 14:35:12 +0200

    added azure logic apps example

---
+++ azure-logic-apps/MasterDeployment.ps1
@@ -0,0 +1,17 @@
+﻿# Example Code Only
+# Provided as-is
+
+param([string]$subscriptionName = "Your Sponsorship", [string]$deploymentType = "Prod", [string]$location = "eastus2", [string]$resourceGroupName="WBC_Production", [string]$localPath="C:\Deployments" )
+
+Login-AzureRmAccount 
+
+$subResult = Select-AzureRmSubscription -SubscriptionName "$subscriptionName"  
+
+# List all the Logic Apps to be deployed in order needed
+$LogicAppList = @("PollWebsites")
+
+Foreach($logicApp in $LogicAppList)
+{
+    $results = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$localPath\$logicApp\WBC.$logicApp.json" -TemplateParameterFile "$localPath\$logicApp\WBC.$logicApp.parameters.$deploymentType.json" 
+    Write-Output $results
+}
